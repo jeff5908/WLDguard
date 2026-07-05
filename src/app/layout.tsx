8ef4d8d0904d398df5c358a05bed3bca74a235a0
Jeff <jeff5908@example.com>
2026-07-05 00:51:56 -0400

    Restored real layout and CSS for Vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,4 @@
-// IMPORTANT: Please UNCOMMENT the line below in your local project before pushing to Vercel!
 // import './globals.css';
-
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -14,13 +12,6 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    /* IMPORTANT: For Next.js and Vercel, you MUST replace the <div> tags below 
-      with standard <html> and <body> tags! 
-      
-      We are temporarily using <div> tags here because this preview environment 
-      renders inside an existing <div>, which causes a DOM nesting error if 
-      an <html> tag is used.
-    */
     <div id="html-mock" lang="en">
       <div id="body-mock" className="bg-slate-950 text-white antialiased min-h-screen">
         {children}
